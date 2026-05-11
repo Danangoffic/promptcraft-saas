cc068227282558b425001451212711379458b689
Danang Arif Rahmanda <darifrahmanda@gmail.com>
2026-05-11 19:38:13 +0700

    Add Midtrans helper

---
+++ lib/midtrans.ts
@@ -0,0 +1,15 @@
+import midtransClient from "midtrans-client";
+
+export function createSnapClient() {
+  return new midtransClient.Snap({
+    isProduction: process.env.MIDTRANS_IS_PRODUCTION === "true",
+    serverKey: process.env.MIDTRANS_SERVER_KEY!,
+    clientKey: process.env.MIDTRANS_CLIENT_KEY!
+  });
+}
+
+export function getMidtransBaseUrl() {
+  return process.env.MIDTRANS_IS_PRODUCTION === "true"
+    ? "https://app.midtrans.com"
+    : "https://app.sandbox.midtrans.com";
+}
